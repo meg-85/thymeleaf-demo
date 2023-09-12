169aed83a851cebce941dd25f3f9222382531fdc
Manuel Egger <manuel.egger85@gmail.com>
2023-09-12 15:05:15 +0200

    add @InitBinder to Controller for removing whitespaces

--- src/main/java/com/example/springboot/thymeleafdemo/Customer.java
+++ src/main/java/com/example/springboot/thymeleafdemo/Customer.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.Size;
 public class Customer {
 
   private String firstName;
+
   @NotNull(message = "is required")
   @Size(min = 1, message = "is required")
   private String lastName;
--- src/main/java/com/example/springboot/thymeleafdemo/controller/CustomerController.java
+++ src/main/java/com/example/springboot/thymeleafdemo/controller/CustomerController.java
@@ -2,27 +2,36 @@ package com.example.springboot.thymeleafdemo.controller;
 
 import com.example.springboot.thymeleafdemo.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
 
+  // add an @InitBinder to trim input strings (remove leading and trailing whitespaces)
+  @InitBinder
+  public void initBinder(WebDataBinder dataBinder) {
+    StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+    dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+  }
+
   @GetMapping("/")
   public String showForm(Model model) {
-
     model.addAttribute("customer", new Customer());
-
     return "customer-form";
   }
 
   @PostMapping("/processForm")
   public String processForm(@Valid @ModelAttribute("customer") Customer customer, BindingResult bindingResult) {
 
+    System.out.println("Last: |" + customer.getLastName() + "|");
     if (bindingResult.hasErrors()) {
       return "customer-form";
     } else {
