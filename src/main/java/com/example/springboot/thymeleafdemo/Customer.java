37d9d9469201859733ccc728c646246e633b69fb
Manuel Egger <manuel.egger85@gmail.com>
2023-09-12 15:48:23 +0200

    added additional fields with validations and a custom error message

--- src/main/java/com/example/springboot/thymeleafdemo/Customer.java
+++ src/main/java/com/example/springboot/thymeleafdemo/Customer.java
@@ -1,9 +1,6 @@
 package com.example.springboot.thymeleafdemo;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -13,15 +10,28 @@ public class Customer {
   @Size(min = 1, message = "is required")
   private String lastName;
 
-  @Min(value=0, message="must be greater than or equal to zero")
-  @Max(value=10, message="must be less than or equal to 10")
-  private int freePasses;
+  @NotNull(message = "is required")
+  @Min(value = 0, message = "must be greater than or equal to zero")
+  @Max(value = 10, message = "must be less than or equal to 10")
+  private Integer freePasses;
 
-  public int getFreePasses() {
+  @NotNull(message = "is required")
+  @Pattern(regexp = "^[0-9]{4,5}", message = "must be 4 or 5 digits")
+  private String postalCode;
+
+  public String getPostalCode() {
+    return postalCode;
+  }
+
+  public void setPostalCode(String postalCode) {
+    this.postalCode = postalCode;
+  }
+
+  public Integer getFreePasses() {
     return freePasses;
   }
 
-  public void setFreePasses(int freePasses) {
+  public void setFreePasses(Integer freePasses) {
     this.freePasses = freePasses;
   }
 
